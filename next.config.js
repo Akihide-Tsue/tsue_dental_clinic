071663859689f6c53f80dd5bea35c2daa99930a1
Akihide-Tsue <akihide.tsue@gmail.com>
2023-11-05 11:20:29 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
